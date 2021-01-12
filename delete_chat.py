cd2593c4ac4f5787184a41315a8cf282c4402b25
Bovonos0 <71797403+Bovonos0@users.noreply.github.com>
2021-01-13 02:45:15 +0300

    Add files via upload
    pkg install git -y
    pkg install python -y
    pkg install python2 -y
    pip3 install Amino.py
    git clone https://github.com/Bovonos0/Delete_Chat
    cd Delete_Chat
    python3 delete_chat.py

---
+++ delete_chat.py
@@ -0,0 +1,24 @@
+import os
+os.system("pip3 install Amino.py")
+os.system("clear")
+import amino
+client=amino.Client()
+email=input("Email: ")
+password=input("Password: ")
+try:
+	client.login(email=email ,password=password)
+except:
+	print ("Email or Password is Incorrect")
+	os._exit(1)
+
+u=input("Host Link: ")
+us=client.get_from_code(u)
+p=us.objectId
+comId=us.path[1:us.path.index('/')]
+chatl=input("Chat Link: ")
+chatId=client.get_from_code(chatl).objectId
+subclient=amino.SubClient(comId=comId, profile=client.profile)
+
+subclient.kick(userId=p ,chatId=chatId, allowRejoin=True)
+print ("Done")
+os._exit(1)
